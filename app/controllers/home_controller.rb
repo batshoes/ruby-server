3bdd9a8aecb5d2f3514522cb913265c2ba0c962f
James Middlemiss <middlemissj.usa@gmail.com>
2016-03-16 10:57:44 -0400

    adds post to routes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
   def index
+    response.headers['Access-Control-Allow-Origin'] = "salemove.com"
+    response.headers['Access-Control-Allow-Credentials'] = "true"
+    
+    # @tickets = BaubleBar::GetZenDeskTickets.new(email_params)
     
   end
 
